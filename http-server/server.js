fc002f91320fb9be1f545bbbd6eb8cefd8629ae3
Rakesh <chipparakesh01@gmail.com>
2024-12-21 21:29:23 +0530

    change build

--- http-server/server.js
+++ http-server/server.js
@@ -1,21 +1,21 @@
-const express = require("express")
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const APP_DIR = '../dist/client/browser'
+const APP_DIR = "../build/";
 
 // console.log(path.join(__dirname, APP_DIR))
 
-const app = express()
+const app = express();
 app.use(express.json({ limit: "1mb" }));
-app.use(express.json())
+app.use(express.json());
 
-app.get('*.*', express.static(path.join(__dirname, APP_DIR)));
-app.all('*', (req, res) => {
-    res.sendFile(path.join(__dirname, APP_DIR, 'index.html'))
-})
+app.get("*.*", express.static(path.join(__dirname, APP_DIR)));
+app.all("*", (req, res) => {
+  res.sendFile(path.join(__dirname, APP_DIR, "index.html"));
+});
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log("server started at http://localhost:" + PORT);
-})
+  console.log("server started at http://localhost:" + PORT);
+});
